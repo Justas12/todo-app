e0a6ae48e43f7006d88936baeeb8c65a488fd3b5
Justas <justjan.softflare@gmail.com>
2020-03-04 13:10:57 +0200

    final fix

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM node:13.8.0-alpine3.11
 
-ENV LANG C.UTF-8
-ENV LC_ALL C.UTF-8
 ENV PORT 80
 
 WORKDIR /usr/src/app
--- web/routes/index.js
+++ web/routes/index.js
@@ -1,24 +1,49 @@
 const express = require("express");
 const Todo = require("../../db/todoModel").Todo;
 const createTodo = require("../../db/todoModel").createTodo;
+const mongoose = require("mongoose");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   Todo.find()
-    .then(todos => res.status(200).send(todos))
-    .catch(rr => res.status(400).send(err.message));
+    .then(todos => {
+      if (todos) {
+        res.status(200).send(todos);
+      }
+      else {
+        res.status(404).send('not found');
+      }
+    })
+    .catch(err => {
+      if (err.message instanceof mongoose.Error.CastError) {
+        res.status(400).send('bad request');
+      }
+      else {
+        res.status(500).send('error fetching data from database');
+      }
+    });
 });
 
 router.get("/:id", (req, res) => {
   const id = req.params.id;
-  Todo.findById(id, (err, todo) => {
-    if (err) {
-      res.status(400).send(err.message);
-    } else {
-      res.status(200).send(todo);
-    }
-  });
+  Todo.findById(id)
+    .then(todo => {
+      if (todo) {
+        res.status(200).send(todo);
+      }
+      else {
+        res.status(404).send('not found');
+      }
+    })
+    .catch(err => {
+      if (err.message instanceof mongoose.Error.CastError) {
+        res.status(400).send('bad request');
+      }
+      else {
+        res.status(500).send('error fetching data from database');
+      }
+    });
 });
 
 router.post("/", (req, res) => {
@@ -28,9 +53,16 @@ router.post("/", (req, res) => {
     todo
       .save(todo)
       .then(newTodo =>
-        res.status(200).send(`successufully created object: ${id}`)
+        res.status(201).send(`successufully created object: ${id}`)
       )
-      .catch(err => res.status(400).send(err));
+      .catch(err => {
+        if (err.message instanceof mongoose.Error.CastError) {
+          res.status(400).send('bad request');
+        }
+        else {
+          res.status(500).send('error saving data to database');
+        }
+      });
   }
   else {
     res.status(400).send(`could not create object`);
@@ -41,7 +73,12 @@ router.put("/:id", (req, res) => {
   const id = req.params.id;
   Todo.findByIdAndUpdate(id, req.body, (err, todo) => {
     if (err) {
-      res.status(400).send(err.message);
+      if (err.message instanceof mongoose.Error.CastError) {
+        res.status(400).send('bad request');
+      }
+      else {
+        res.status(500).send('error saving data to database');
+      }
     } else {
       res.status(200).send(`successufully updated object: ${id}`);
     }
@@ -51,7 +88,12 @@ router.put("/:id", (req, res) => {
 router.delete("/", (req, res) => {
   Todo.deleteMany({}, (err, todo) => {
     if (err) {
-      res.status(400).send(err.message);
+      if (err.message instanceof mongoose.Error.CastError) {
+        res.status(400).send('bad request');
+      }
+      else {
+        res.status(500).send('error deleting data from database');
+      }
     } else {
       res.status(200).send(`successufully deleted all objects!`);
     }
@@ -62,7 +104,12 @@ router.delete("/:id", (req, res) => {
   const id = req.params.id;
   Todo.findByIdAndDelete(id, (err, todo) => {
     if (err) {
-      res.status(400).send(err.message);
+      if (err.message instanceof mongoose.Error.CastError) {
+        res.status(400).send('bad request');
+      }
+      else {
+        res.status(500).send('error deleting data from database');
+      }
     } else {
       res.status(200).send(`successufully deleted object: ${id}`);
     }
