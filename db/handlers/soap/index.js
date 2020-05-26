aff48a2d6cb624f23dac99644e77c979a88ff1ff
Justas <justasjanusis2@gmail.com>
2020-05-27 00:19:57 +0300

    added soap

---
+++ db/handlers/soap/index.js
@@ -0,0 +1,70 @@
+const Todo = require("../../models/todo").Todo;
+
+exports.getTodos = async () => {
+	var arr = [];
+	const result = await Todo.find().exec();
+	console.log(result);
+	result.forEach(el => {
+		arr.push({
+			id: el._id.toString(),
+			text: el.text,
+			due: el.due,
+			patient: el.patient,
+			completed: el.completed,
+		});
+	});
+	return arr;
+}
+
+exports.getTodo = async (args) => {
+	const result = await Todo.findById(args.id).exec();
+	console.log(result);
+	return {
+		id: result._id.toString(),
+		text: result.text,
+		due: result.due,
+		patient: result.patient,
+		completed: result.completed,
+	};
+}
+
+exports.addTodo = async (args) => {
+	const result = await new Todo(args).save();
+	console.log(result);
+	return {
+		id: result._id.toString(),
+		text: result.text,
+		due: result.due,
+		patient: result.patient,
+		completed: result.completed,
+	}
+}
+
+exports.deleteTodos = async () => {
+	let deletedCount = 0;
+	const result = await Todo.deleteMany({}).exec();
+	console.log(result);
+	deletedCount = result.deletedCount;
+	return {
+		deleted: deletedCount,
+	};
+}
+
+exports.deleteTodo = async (args) => {
+	const result = Todo.findByIdAndDelete(args.id).exec();
+	console.log(result);
+	return;
+}
+
+exports.updateTodo = async (args) => {
+	let result;
+	const id = args.id;
+	delete args.id;
+	if (Object.keys(args).length === 0) {
+		result = await Todo.findById(id).exec();
+		result = await Todo.updateOne({ _id: id }, { completed: !result.completed }).exec();
+	} else {
+		result = await Todo.updateOne({ _id: id }, { $set: args }).exec();
+	}
+	console.log(result);
+}
--- web/index.js
+++ web/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const routes = require("./routes");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const fs = require("fs");
+const soap = require("soap");
+const soapHandlers = require("../db/handlers/soap");
 
 const app = express();
 
@@ -17,12 +20,31 @@ app.use(jsonParser);
 app.use("/todos", routes.todosRouter);
 app.use("/", routes.mainRouter);
 
+var service = {
+  TodoService: {
+    TodoServiceSoapPort: {
+      GetTodos: soapHandlers.getTodos,
+      GetTodo: soapHandlers.getTodo,
+      AddTodo: soapHandlers.addTodo,
+      DeleteTodos: soapHandlers.deleteTodos,
+      DeleteTodo: soapHandlers.deleteTodo,
+      UpdateTodo: soapHandlers.updateTodo,
+    },
+  },
+};
+
+var xml = fs.readFileSync("service.wsdl", "utf8");
+
 mongoose
   .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("APP IS RUNNING!");
-    app.listen(port, () =>
-      console.log(`Listening on http://127.0.0.1:${port}`)
+    app.listen(port, () => {
+      console.log(`Listening on http://127.0.0.1:${port}`);
+      soap.listen(app, "/wsdl", service, xml, () => {
+        console.log("server initialized!");
+      });
+      }
     );
   })
   .catch(err => {
